f3bfb9ec5aea1e84929e31de1ea0539a8f34cb69
krishna <kykrishna24@gmail.com>
2025-07-12 02:21:44 +0530

    Update CODPayment.java

--- src/main/java/com/mc/lld/service/CODPayment.java
+++ src/main/java/com/mc/lld/service/CODPayment.java
@@ -3,7 +3,7 @@ package com.mc.lld.service;
 public class CODPayment implements PaymentProcessor {
     @Override
     public boolean processPayment(Double value) {
-        System.out.println("Successfully processed payment for COD payment mode., value= " + value);
+        System.out.println("Successfully processed payment for COD payment mode., value= ");
         return true;
     }
 }
